27151b18eef10ec2df540092f8c34b462f22ba91
seoariseo <seoari704@gmail.com>
2024-07-15 17:45:54 +0900

    hollys_1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+    $(".main > li, .sub_bg").hover(function(){
+        $(".sub, .sub_bg").stop().slideDown();
+    },function(){
+        $(".sub, .sub_bg").stop().slideUp();
+    })//hover
+
+
+    //slick
+
+$(".move").slick({
+    autoplay:true,
+    dots:true,
+    arrows:false,
+    autoplaySpeed:2000,
+    adaptiveHeight:true
+})
+
+
+})//jquery
